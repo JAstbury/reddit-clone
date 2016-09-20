feb2d53f9e010cb60aa42f918e5592612e221b57
JAstbury <jess.astbury@hotmail.com>
2016-09-20 23:33:58 +0100

    user can leave a comment

--- app.js
+++ app.js
@@ -62,5 +62,15 @@ app.controller('MainCtrl', [
     function($scope, $stateParams, posts){
 
       $scope.post = posts.posts[$stateParams.id];
-      
+
+      $scope.addComment = function() {
+        if($scope.body === '') { return; }
+          $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+          });
+    $scope.body = '';
+  };
+
   }]);
